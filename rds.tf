a4c99cf2ac294913eada60d0943d1b7744574bab
Ravi <ravi@gmail.com>
2022-10-11 10:27:45 +0530

    ec2 code

--- rds.tf
+++ rds.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "default" {
   engine               = var.RDS_ENGINE
   engine_version       = var.RDS_ENGINE_VERSION
   instance_class       = var.RDS_INSTANCE_CLASS
-  username             = "admin"
-  password             = "admin123"
+  username             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
+  password             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]
   parameter_group_name = aws_db_parameter_group.main.name
   vpc_security_group_ids = [aws_security_group.main.id]
   db_subnet_group_name = aws_db_subnet_group.main.name
