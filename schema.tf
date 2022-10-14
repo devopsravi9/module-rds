754514b5cd1b86c496d94848931562dab77e8a75
Ravi <ravi@gmail.com>
2022-10-14 16:57:09 +0530

    ec2 code

--- schema.tf
+++ schema.tf
@@ -7,7 +7,7 @@ cd /tmp
 unzip -o mysql.zip
 cd mysql-main
 
-mysql -u ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]} -p ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]} -h ${aws_db_instance.default.address} <shipping.sql
+mysql -u${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]} -p${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]} -h ${aws_db_instance.default.address} <shipping.sql
 
 EOF
   }
