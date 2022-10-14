64309187bf77940f5dcf30ce30b38da46aa47559
Ravi <ravi@gmail.com>
2022-10-14 12:18:20 +0530

    ec2 code

---
+++ schema.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "example2" {
+  provisioner "local-exec" {
+    command = <<EOF
+
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+
+mysql -u ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]} -p ${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]} -h ${aws_db_instance.default.address} <shipping.sql
+
+EOF
+  }
+}
+
+
